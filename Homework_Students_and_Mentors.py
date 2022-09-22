178603ffe84d823e1502d0056ef20be7ab1b636e
Alexey Uliyanov <prairie68@gmail.com>
2022-09-22 11:08:14 +0300

    Task 3 added

--- Homework_Students_and_Mentors.py
+++ Homework_Students_and_Mentors.py
@@ -36,7 +36,17 @@ class Student:
         if not isinstance(other, Student):
             print('Нет в списке студентов')
             return
-        return self.__av_grade < other.__av_grade
+        sum_av_self = 0
+        for course in self.grades:
+            course_av_self = sum(self.grades.get(course)) / len(self.grades.get(course))
+            sum_av_self += course_av_self
+        res_self = round((sum_av_self / len(self.grades)), 1)
+        sum_av_other = 0
+        for course in other.grades:
+            course_av_other = sum(other.grades.get(course)) / len(other.grades.get(course))
+            sum_av_other += course_av_other
+        res_other = round((sum_av_other / len(other.grades)), 1)
+        return res_self > res_other
 
     def __str__(self):
         res = f"""Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашние задания: {self.__av_grade()}
@@ -98,10 +108,10 @@ petr_vasiliev = Student('Пётр', 'Васильев', 'муж')
 petr_vasiliev.add_courses('Stage_1')
 petr_vasiliev.grades = {'Stage_1': [8, 9, 10]}
 petr_vasiliev.courses_in_progress = ['Stage_2', 'Stage_3']
-# petr_vasiliev.rate_lec(svetlana_stepanova, 'Stage_2', 10)
 
 olga_smirnova = Student('Ольга', 'Смирнова', 'жен')
-olga_smirnova.finished_courses = ['Stage_1', 'Stage_2']
+olga_smirnova.add_courses('Stage_1')
+olga_smirnova.add_courses('Stage_2')
 olga_smirnova.grades = {'Stage_1': [10, 9, 10], 'Stage_2': [9, 10]}
 olga_smirnova.courses_in_progress = ['Stage_3', 'Stage_4']
 
@@ -113,12 +123,24 @@ svetlana_stepanova.courses_attached = ['Stage_2', 'Stage_4']
 
 natalia_orlova = Reviewer('Наталья', 'Орлова')
 natalia_orlova.courses_attached = ['Stage_1', 'Stage_2']
+
+vladimir_volkov = Reviewer('Владимир', 'Волков')
+vladimir_volkov.courses_attached = ['Stage_1', 'Stage_2', 'Stage_3', 'Stage_4']
+
+petr_vasiliev.rate_lec(sergey_semenov, 'Stage_3', 8)
+petr_vasiliev.rate_lec(sergey_semenov, 'Stage_3', 9)
+olga_smirnova.rate_lec(sergey_semenov, 'Stage_3', 9)
+olga_smirnova.rate_lec(sergey_semenov, 'Stage_3', 10)
+
+petr_vasiliev.rate_lec(svetlana_stepanova, 'Stage_2', 10)
+petr_vasiliev.rate_lec(svetlana_stepanova, 'Stage_2', 8)
+olga_smirnova.rate_lec(svetlana_stepanova, 'Stage_4', 10)
+olga_smirnova.rate_lec(svetlana_stepanova, 'Stage_4', 10)
+
 natalia_orlova.rate_hw(petr_vasiliev, 'Stage_2', 8)
 natalia_orlova.rate_hw(petr_vasiliev, 'Stage_2', 9)
 natalia_orlova.rate_hw(petr_vasiliev, 'Stage_2', 9)
 
-vladimir_volkov = Reviewer('Владимир', 'Волков')
-vladimir_volkov.courses_attached = ['Stage_1', 'Stage_2', 'Stage_3', 'Stage_4']
 vladimir_volkov.rate_hw(olga_smirnova, 'Stage_3', 10)
 vladimir_volkov.rate_hw(olga_smirnova, 'Stage_3', 9)
 vladimir_volkov.rate_hw(olga_smirnova, 'Stage_3', 9)
@@ -129,8 +151,6 @@ print(petr_vasiliev)
 print()
 print(olga_smirnova)
 print()
-print(petr_vasiliev < olga_smirnova)
-print()
 print(sergey_semenov)
 print()
 print(svetlana_stepanova)
@@ -138,3 +158,6 @@ print()
 print(vladimir_volkov)
 print()
 print(natalia_orlova)
+print()
+print(petr_vasiliev < olga_smirnova)
+
